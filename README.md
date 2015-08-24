273da399163b183fce257caf11e9ba5585b7843f
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2015-08-24 12:34:27 +0500

    new: setup.sh

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # OSM Bright 2 source
 
+## Description
+
+OSM Bright 2 Mapbox Studio source
+
 ![thumb](/.thumb.png?raw=true "thumb")
+
+## Dependencies
+
+* PostgreSQL
+* imposm
+
+## Setup
+
+Setup database
+
+```shell
+./setup.sh <database>
+```
+
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+if [ -z "$1" ]; then
+   echo "Usage: ./setup.sh <database>"
+   exit 1
+fi
+
+db=$1
+cachedir="cache_$1"
+
+urls=('http://download.geofabrik.de/europe/russia-european-part-latest.osm.pbf'
+      'http://download.geofabrik.de/asia/russia-asian-part-latest.osm.pbf')
+
+for url in "${urls[@]}"
+do
+  filename=$(basename $url)
+  echo "${green}[*] Downloading $url${reset}"
+  curl -O $url
+  echo "${green}[*] Importing $filename${reset}"
+
+  ~/venv/bin/imposm -d $db \
+                    -m imposm-mapping.py \
+                    --cache-dir=$cachedir \
+                    --read \
+                    --write \
+                    --merge-cache \
+                    --optimize \
+                    --deploy-production-tables \
+                    $filename
+done
+
+echo "${green}[*] Done${reset}"
